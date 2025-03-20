cce67aa675924e53a6b5a1889dce59a279557403
Jeff Snow <jeff.wons@gmail.com>
2025-03-20 10:54:31 +0700

    not being accessed apiurl

--- src/api/thai2englishService.ts
+++ src/api/thai2englishService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL =
-  import.meta.env.VITE_API_URL?.replace(/\/$/, "") || "http://localhost:3000";
+// const API_URL =
+//   import.meta.env.VITE_API_URL?.replace(/\/$/, "") || "http://localhost:3000";
 
 interface Meaning {
   meaning: string;

60a5eae6374e9df87c9bd318efbfd76defe4f248
Jeff Snow <jeff.wons@gmail.com>
2025-03-21 18:45:21 +0700

    linting errors

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,10 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  browserPopupRedirectResolver,
-  connectAuthEmulator,
-} from "firebase/auth";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
